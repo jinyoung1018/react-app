a667a923af600350eb7d4c5d276bd4ac1a5976c7
jinyoung1018 <jywsb1816@gmail.com>
2023-06-12 15:12:47 +0900

    컴포넌트 기본

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+
+// function Subject(){
+//   return (
+//     <div className='Subject'>
+//       <header>
+//         <h1>WEB</h1>
+//         world wide web!
+//       </header>
+//     </div>
+//   );
+// }
+
+class Subject extends Component{
+  render(){
+    return (
+      <header>
+        <h1>WEB</h1>
+        world wide web!
+      </header>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      Hello, React!
+      <Subject></Subject>
     </div>
   );
 }
