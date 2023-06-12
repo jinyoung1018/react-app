806ad9ab1f621cfa1ee9f50ce504430a0990e9a5
jinyoung1018 <jywsb1816@gmail.com>
2023-06-12 15:23:12 +0900

    컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -24,10 +24,40 @@ class Subject extends Component{
   }
 }
 
+class TOC extends Component{
+  render(){
+    return(
+      <nav>
+        <ul>
+          <li><a href="1.html">HTML</a></li>
+          <li><a href="2.html">CSS</a></li>
+          <li><a href="3.html">JavaScript</a></li>
+
+        </ul>
+      </nav>
+    )
+  }
+
+}
+
+class Content extends Component{
+  render(){
+    return (
+      <article>
+        <h2>HTML</h2>
+        HTML is HyperText Markup Language.
+      </article>
+    )
+  }
+
+}
+
 function App() {
   return (
     <div className="App">
       <Subject></Subject>
+      <TOC></TOC>
+      <Content></Content>
     </div>
   );
 }
